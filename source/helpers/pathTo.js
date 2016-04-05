b99e48313449c530d254b2f03fb1be5de15d3852
Dmitry Yakimov <welldan97@gmail.com>
2016-04-05 20:13:14 +0300

    Little bit of styles and fix main page

--- source/helpers/pathTo.js
+++ source/helpers/pathTo.js
@@ -1,15 +1,14 @@
 'use strict';
 
-module.exports = (path, text, options) => {
-  // HACK: Use this.pathTo
+module.exports = (path, isPage) => {
   let root = '';
   let suffix = '';
   if(process.env.NODE_ENV === 'production') {
     root = 'http://beadwall.github.io/fujisan';
   } else {
-    if(path !== '/') {
+    if(path !== '/' && isPage) {
       suffix = '.html';
     }
   }
-   return `<a class="${options.class}" href="${root + path + suffix}">${text}</a>`;
+  return root + path + suffix;
 };
